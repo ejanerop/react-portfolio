2a672df95f4b1d0f0598fdfb318902584e95c21a
Eric Janero <ejanerop97@gmail.com>
2022-03-21 13:52:09 -0400

    class for className in about component

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -4,21 +4,21 @@ import Picture from "./../../eric.jpg";
 export default function About() {
     return (
         <section id="about-section">
-            <div class="px-4 py-5">
-                <div class="row g-5 py-5 about-container">
-                    <div class="col-8 col-sm-8 col-md-6 col-lg-3 col-xl-3 image-container">
+            <div className="px-4 py-5">
+                <div className="row g-5 py-5 about-container">
+                    <div className="col-8 col-sm-8 col-md-6 col-lg-3 col-xl-3 image-container">
                         <img
                             src={Picture}
-                            class="d-block mx-lg-auto img-fluid portfolio-img"
+                            className="d-block mx-lg-auto img-fluid portfolio-img"
                             alt="Bootstrap Themes"
                             width="700"
                             height="500"
                             loading="lazy"
                         />
                     </div>
-                    <div class="col-lg-6 about-info">
-                        <h1 class="display-5 lh-1 mb-3">About me</h1>
-                        <p class="lead">
+                    <div className="col-lg-6 about-info">
+                        <h1 className="display-5 lh-1 mb-3">About me</h1>
+                        <p className="lead">
                             I'm a software engineer graduated from the
                             Technological University of Havana (CUJAE) in
                             December 2020. Excellent communication and teamwork
