0b95422f3dc74b169f6843f41bc7303df0e7d528
Eric Janero <ejanerop97@gmail.com>
2022-03-21 13:52:52 -0400

    added background when scrolling to navbar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,8 +1,30 @@
+import {useEffect, useState} from "react";
 import "./navbar.component.scss";
 
-export default function Navbar() {
+const Navbar = () => {
+    const [navbar, setNavbar] = useState(false);
+
+    const changeBackground = () => {
+        if (window.scrollY >= 66) {
+            setNavbar(true);
+        } else {
+            setNavbar(false);
+        }
+    };
+
+    useEffect(() => {
+        changeBackground();
+        // adding the event when scroll change background
+        window.addEventListener("scroll", changeBackground);
+    });
+
     return (
-        <nav className="navbar navbar-expand-lg fixed-top navbarScroll">
+        <nav
+            className={
+                navbar
+                    ? "navbar active navbar-expand-lg fixed-top navbarScroll"
+                    : "navbar navbar-expand-lg fixed-top navbarScroll"
+            }>
             <div className="container">
                 <a className="navbar-brand" href="/">
                     Eric
@@ -27,23 +49,18 @@ export default function Navbar() {
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#about">
+                            <a className="nav-link" href="#about-section">
                                 About
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#services">
-                                Services
+                            <a className="nav-link" href="#skills-section">
+                                Skills
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#portfolio">
-                                Portfolio
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#contact">
-                                Contact
+                            <a className="nav-link" href="#experience-section">
+                                Experience
                             </a>
                         </li>
                     </ul>
@@ -51,4 +68,6 @@ export default function Navbar() {
             </div>
         </nav>
     );
-}
+};
+
+export default Navbar;
