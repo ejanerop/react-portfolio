fa9b0fe49a494d1a7c1f03d3698d7c19d6b180dc
Eric Janero <ejanerop97@gmail.com>
2022-03-20 22:03:34 -0400

    Created experience component

---
+++ src/components/experience/experiece.component.jsx
@@ -0,0 +1,48 @@
+import "./experience.component.scss";
+import {
+    VerticalTimeline,
+    VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+export default function Experience() {
+    return (
+        <section id="experience-section">
+            <h1 class="display-5 lh-1 mb-5">Experience</h1>
+            <VerticalTimeline lineColor={"#1F1F1F"}>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--education"
+                    date="Mar. 2021 - Present"
+                    iconStyle={{background: "rgb(108,117,125)", color: "#fff"}}
+                    icon={<i className="fas fa-code experience-icon"></i>}>
+                    <h3 className="vertical-timeline-element-title mb-2">
+                        Software Engineer
+                    </h3>
+                    <span className="vertical-timeline-element-subtitle mb-3">
+                        Dofleini Software
+                    </span>
+                    <div className="mt-1">
+                        <span className="experience-badge me-2 mb-2 badge bg-secondary">
+                            Grails
+                        </span>
+                        <span className="experience-badge me-2 mb-2 badge bg-secondary">
+                            jQuery
+                        </span>
+                        <span className="experience-badge me-2 mb-2 badge bg-secondary">
+                            Bootstrap
+                        </span>
+                        <span className="experience-badge me-2 mb-2 badge bg-secondary">
+                            MySQL
+                        </span>
+                    </div>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    iconStyle={{background: "rgb(108,117,125)", color: "#fff"}}
+                    icon={
+                        <i className="fas fa-hourglass-start experience-icon"></i>
+                    }
+                />
+            </VerticalTimeline>
+        </section>
+    );
+}
