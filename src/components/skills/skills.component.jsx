3cf810bd519598e679523076fb1384cac9291a3d
Eric Janero <ejanerop97@gmail.com>
2022-03-28 01:15:23 -0400

    map skills

--- src/components/skills/skills.component.jsx
+++ src/components/skills/skills.component.jsx
@@ -1,42 +1,50 @@
 import "./skills.component.scss";
 
 export default function Skills() {
+    let skills = [
+        {
+            name: "HTML",
+            icon: "fab fa-html5",
+        },
+        {
+            name: "CSS",
+            icon: "fab fa-css3-alt",
+        },
+        {
+            name: "Javascript",
+            icon: "fab fa-js",
+        },
+        {
+            name: "PHP",
+            icon: "fab fa-php",
+        },
+        {
+            name: "Laravel",
+            icon: "fab fa-laravel",
+        },
+        {
+            name: "Angular",
+            icon: "fab fa-angular",
+        },
+        {
+            name: "Java",
+            icon: "fab fa-java",
+        },
+        {
+            name: "Git",
+            icon: "fab fa-git-alt",
+        },
+    ];
     return (
         <section id="skills-section">
             <h1 className="display-5 lh-1 mb-3">My top skills</h1>
             <div className="skills-container">
-                <div className="col-12 col-md-3 skill">
-                    <i className="fab fa-html5"></i>
-                    <h3 className="skill-name">HTML</h3>
-                </div>
-                <div className="col-12 col-md-3 skill">
-                    <i className="fab fa-css3-alt"></i>
-                    <h3 className="skill-name">CSS</h3>
-                </div>
-                <div className="col-12 col-md-3 skill">
-                    <i className="fab fa-js"></i>
-                    <h3 className="skill-name">Javascript</h3>
-                </div>
-                <div className="col-12 col-md-3 skill">
-                    <i className="fab fa-php"></i>
-                    <h3 className="skill-name">PHP</h3>
-                </div>
-                <div className="col-12 col-md-3 skill">
-                    <i className="fab fa-laravel"></i>
-                    <h3 className="skill-name">Laravel</h3>
-                </div>
-                <div className="col-12 col-md-3 skill">
-                    <i className="fab fa-angular"></i>
-                    <h3 className="skill-name">Angular</h3>
-                </div>
-                <div className="col-12 col-md-3 skill">
-                    <i className="fab fa-java"></i>
-                    <h3 className="skill-name">Java</h3>
-                </div>
-                <div className="col-12 col-md-3 skill">
-                    <i className="fab fa-git-alt"></i>
-                    <h3 className="skill-name">Git</h3>
-                </div>
+                {skills.map((skill, index) => (
+                    <div key={index} className="col-12 col-md-3 skill">
+                        <i className={skill.icon}></i>
+                        <h3 className="skill-name">{skill.name}</h3>
+                    </div>
+                ))}
             </div>
         </section>
     );
