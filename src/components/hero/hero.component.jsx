6fa259f1176a42333d1375220bf22d34da9813fd
Eric Janero <ejanerop97@gmail.com>
2022-04-04 13:11:16 -0400

    test

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -1,8 +1,10 @@
 import "./hero.component.scss";
+import Thumb from "../../assets/thumb_img.webp";
 
 export default function Hero() {
     return (
         <section className="bgimage" id="home">
+            {<img src={Thumb} alt="asd" />}
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
