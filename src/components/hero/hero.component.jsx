75d2f9454ce1a93f41ae1105f94add430caab43d
Eric Janero <ejanerop97@gmail.com>
2022-04-04 13:13:40 -0400

    Added meta img

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -4,7 +4,6 @@ import Thumb from "../../assets/thumb_img.webp";
 export default function Hero() {
     return (
         <section className="bgimage" id="home">
-            {<img src={Thumb} alt="asd" />}
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
